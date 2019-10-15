e3659a031be0e371a33fcea301f2ad5b6e735628
Tim <40692433+Osipchik@users.noreply.github.com>
2019-10-15 18:23:07 +0300

    Update MSISVIFT.cpp

--- Third term/MSISVIFT.cpp
+++ Third term/MSISVIFT.cpp
@@ -1,83 +1,23 @@
-#include <iostream>
-using namespace std;
-
-struct Matrix {
-	int columns;
-	int rows;
-	int** array;
-	int value;
-	int valueColumn;
-	int valueRow;
-};
-
-void SetValue(Matrix& matrix, int currentRow, int currentCol, bool max) {
-	cin >> matrix.array[currentRow][currentCol];
-	if (max ? matrix.value < matrix.array[currentRow][currentCol] : matrix.value > matrix.array[currentRow][currentCol]
-		|| matrix.value == -1) {
-		matrix.value = matrix.array[currentRow][currentCol];
-		matrix.valueRow = currentRow;
-		matrix.valueColumn = currentCol;
-	}
-}
-
-Matrix CreateMatrix(bool findMin, int rows = 0) {
-	int row, col;
-	if (rows == 0) cin >> row;
-	else row = rows;
-	cin >> col;
-	Matrix matrix = { col, row, new int* [row], -1 };
-	for (int i = 0; i < matrix.rows; i++) {
-		matrix.array[i] = new int[matrix.columns];
-		for (int j = 0; j < col; j++) {
-			SetValue(matrix, i, j, findMin);
-		}
-	}
-
-	return matrix;
-}
-
-void SwapValue(Matrix& matrixA, Matrix& matrixB) {
-	for (int i = 0; i < matrixA.columns; i++) {
-		int rowValue = matrixA.array[matrixA.valueRow][i];
-		int colValue = matrixB.array[i][matrixB.valueColumn];
-		matrixA.array[matrixA.valueRow][i] = colValue;
-		matrixB.array[i][matrixB.valueColumn] = rowValue;
-	}
-}
-
-void PrintMatrix(Matrix matrix, bool showValueInfo) {
-	cout << "\n";
-	for (int i = 0; i < matrix.rows; i++) {
-		for (int j = 0; j < matrix.columns; j++) {
-			cout << matrix.array[i][j] << "  ";
-		}
-		cout << "\n";
-	}
-
-	if (showValueInfo) {
-		cout << "value = " << matrix.value << ", positionX = "
-			 << matrix.valueColumn << ", positionY = " << matrix.valueRow << "\n";
-	}
-	cout << "\n";
-}
-
-void DeleteMatrix(Matrix& matrix)
-{
-	for (int i = 0; i < matrix.rows; i++) {
-		delete[] matrix.array[i];
-	}
-	delete[] matrix.array;
-}
-
 int main()
 {
-	Matrix matrixA = CreateMatrix(true);
-	PrintMatrix(matrixA, true);
-	Matrix matrixB = CreateMatrix(false, matrixA.columns);
-	PrintMatrix(matrixB, true);
-	SwapValue(matrixA, matrixB);
-	PrintMatrix(matrixA, false);
-	PrintMatrix(matrixB, false);
-	DeleteMatrix(matrixA);
-	DeleteMatrix(matrixB);
+	setlocale(LC_ALL, "Russian");
+	int array[100], last = 0;
+	for (int i = 0; i < 100; i++) {
+		cin >> array[i];
+	}
+
+	int count = 0;
+	for (int i = 0; i < 100 && count < 10; i++){
+		if (array[i] > 15) {
+			cout << "значение: " << array[i] << "\tпозиция: " << i << "\n";
+			count++;
+		}
+	}
+
+	if (count == 0) {
+		cout << "Таких значений нет";
+	}
+	else if (count < 10) {
+		cout << "Таких значений больше нет";
+	}
 }
