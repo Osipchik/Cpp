40f33581dbe82c079bcc1ba7d2457744ddadb4fc
Tim <40692433+Osipchik@users.noreply.github.com>
2019-10-16 17:09:31 +0300

    Update MSISVIFT.cpp

--- Third term/MSISVIFT.cpp
+++ Third term/MSISVIFT.cpp
@@ -1,23 +1,36 @@
+#include <iostream>
+using namespace std;
+
 int main()
 {
 	setlocale(LC_ALL, "Russian");
-	int array[100], last = 0;
-	for (int i = 0; i < 100; i++) {
+	const int compareValue = 15;
+	const int arrayLength = 100;
+	const int zeroValue = 0;
+	const int maxCount = 10;
+	const char zeroTextOutput[] = "Таких значений нет";
+	const char notFullTextOutput[] = "Таких значений больше нет";
+	const char valueTextOutput[] = "значение: ";
+	const char valuePositionTextOutput[] = "\tпозиция: ";
+
+
+	int array[arrayLength];
+	for (int i = zeroValue; i < arrayLength; i++) {
 		cin >> array[i];
 	}
 
-	int count = 0;
-	for (int i = 0; i < 100 && count < 10; i++){
-		if (array[i] > 15) {
-			cout << "значение: " << array[i] << "\tпозиция: " << i << "\n";
+	int count = zeroValue;
+	for (int i = zeroValue; i < arrayLength && count < maxCount; i++){
+		if (array[i] > compareValue) {
+			cout << valueTextOutput << array[i] << valuePositionTextOutput << i << "\n";
 			count++;
 		}
 	}
 
-	if (count == 0) {
-		cout << "Таких значений нет";
+	if (count == zeroValue) {
+		cout << zeroTextOutput;
 	}
-	else if (count < 10) {
-		cout << "Таких значений больше нет";
+	else if (count < maxCount) {
+		cout << notFullTextOutput;
 	}
 }
