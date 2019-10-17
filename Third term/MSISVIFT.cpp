01046048f6215ec45d4546f1eedf9bbeb08db416
Tim <40692433+Osipchik@users.noreply.github.com>
2019-10-17 17:35:40 +0300

    Update MSISVIFT.cpp

--- Third term/MSISVIFT.cpp
+++ Third term/MSISVIFT.cpp
@@ -6,28 +6,27 @@ int main()
 	setlocale(LC_ALL, "Russian");
 	const int compareValue = 15;
 	const int arrayLength = 100;
-	const int zeroValue = 0;
 	const int maxCount = 10;
 	const char zeroTextOutput[] = "Таких значений нет";
 	const char notFullTextOutput[] = "Таких значений больше нет";
 	const char valueTextOutput[] = "значение: ";
 	const char valuePositionTextOutput[] = "\tпозиция: ";
 
-
 	int array[arrayLength];
-	for (int i = zeroValue; i < arrayLength; i++) {
+	int count = 0;
+
+	for (int i = 0; i < arrayLength; i++) {
 		cin >> array[i];
 	}
 
-	int count = zeroValue;
-	for (int i = zeroValue; i < arrayLength && count < maxCount; i++){
+	for (int i = 0; i < arrayLength && count < maxCount; i++){
 		if (array[i] > compareValue) {
 			cout << valueTextOutput << array[i] << valuePositionTextOutput << i << "\n";
 			count++;
 		}
 	}
 
-	if (count == zeroValue) {
+	if (count == 0) {
 		cout << zeroTextOutput;
 	}
 	else if (count < maxCount) {
